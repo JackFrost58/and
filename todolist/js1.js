b53f9b2399de37515f91ae591aab932c6bbc6cda
Vladislav <33397907+JackFrost58@users.noreply.github.com>
2017-11-13 15:14:24 +0300

    Update js1.js

--- todolist/js1.js
+++ todolist/js1.js
@@ -1,44 +1,63 @@
 function DeleteTask(e){
    var elem = document.getElementById(e.id);
 return elem.parentNode.removeChild(elem)
+   
+}
+var n = 0;
+
+var btnadd = document.getElementById('B_add');
+
+btnadd.onclick = function(){
+   NewRecord();
 }
 
-var n = 0;
+var ul =  document.getElementById('list');
+
 function NewRecord() {
   var InputRecord = document.getElementById("place").value;
-     if(InputRecord === '' || InputRecord.search(/\s/g) === 0 ) { 
+    if(InputRecord === '' || InputRecord.search(/\s/g) === 0 ) { 
         document.getElementById("place").value = '';
-        alert('Введите задачу');
-     }
+        alert('Введите задачу')}
      else {
-        var element =  document.getElementById('list');
-        var item = document.createElement('li');
-        var btn = document.createElement('input');
-        var checkbox = document.createElement('input');
-         checkbox.id = n;
-         checkbox.type = 'checkbox';
-         checkbox.setAttribute('onclick','TaskComp(this)');
-         item.id =n;
-         btn.id = n;
-         btn.className = 'doc';
-         btn.type = 'button';
-         btn.value = 'Delete';
-         btn.setAttribute('onclick','DeleteTask(this)');
-         item.innerHTML = InputRecord;
-         element.appendChild(item);
-         item.appendChild(btn);
-         item.appendChild(checkbox);
+        var task = document.createElement('li');
+         task.id = n;
+         task.innerHTML = InputRecord;
+         ul.appendChild(task);
+         task.appendChild(btnDel());
+         task.appendChild(checkbox());
          document.getElementById("place").value = "";
          n = ++n;
      }   
     }
 
+function btnDel(){
+    var btn = document.createElement('input');
+         btn.id = n;
+         btn.className = 'doc';
+         btn.type = 'button';
+         btn.value = 'Delete';
+         btn.onclick = function(){
+             DeleteTask(this);
+         }
+         return btn;
+}
+
+function checkbox(){
+    var checkbox = document.createElement('input');
+         checkbox.id = n;
+         checkbox.type = 'checkbox';
+         checkbox.onclick =function(){
+             TaskComp(this);
+         }
+       return checkbox;
+}
+
 function TaskComp (box) {
+    
     if(box.checked){
-        document.getElementById(box.id).style.textDecoration = "line-through";
+      document.getElementById(box.id).style.textDecoration = "line-through";
     }
     else
-    {
-        document.getElementById(box.id).style.textDecoration = "none";
+    {        document.getElementById(box.id).style.textDecoration = "none";
         }  
 }
